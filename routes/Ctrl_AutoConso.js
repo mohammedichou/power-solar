a4f3ccc0cd401baf0766df1e7a6bfe960db31de7
Devmohammed <mohammed.ichou@hotmail.fr>
2020-03-25 18:53:51 +0100

    modif live 3

--- routes/Ctrl_AutoConso.js
+++ routes/Ctrl_AutoConso.js
@@ -127,7 +127,11 @@ function createObjTab(){
             if(i == 0){
                 GainAutoConso.push(Gain_conso_annuel + Gain_revente_annul + (PRIME / 5))
             }else{
-                GainAutoConso.push(GainAutoConso[i-1] + (alpha * Gain_conso_annuel) + Gain_revente_annul + (PRIME / 5))
+                if(i<=5){
+                    GainAutoConso.push(GainAutoConso[i-1] + (alpha * Gain_conso_annuel) + Gain_revente_annul + (PRIME / 5))
+                }else{
+                    GainAutoConso.push(GainAutoConso[i-1] + (alpha * Gain_conso_annuel) + Gain_revente_annul)
+                }
                 alpha += 0.03
             }   
         }
